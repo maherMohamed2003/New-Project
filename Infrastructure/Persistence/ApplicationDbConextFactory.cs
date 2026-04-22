7b4a4fdd24159d0bcdec45422850c22f6b91ec0d
Your Full Name <you@example.com>
2026-04-22 13:32:40 +0200

    Initail Commit

---
+++ Infrastructure/Persistence/ApplicationDbConextFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new ApplicationDbContext(optionsBuilder.Options, new HttpContextAccessor());
+        }
+    }
+}
---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fluent.Infrastructure.FluentModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        private readonly IHttpContextAccessor _httpContext;
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> dbContextOptions,
+            IHttpContextAccessor httpContextAccessor) : base(dbContextOptions)
+        {
+            _httpContext = httpContextAccessor;
+        }
+
+    }
+}
