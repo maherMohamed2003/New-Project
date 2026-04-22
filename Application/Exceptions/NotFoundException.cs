20465988a11db9de0b0d0d49c191b0fdd519d7ff
Ahmed Momtaz Mohamed <am7883@fayoum.edu.eg>
2026-04-22 15:34:17 +0200

    add application  Structure

---
+++ Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key)
+            : base($"{name} with key ({key}) was not found.") { }
+    }
+}
---
+++ Application/Exceptions/ValidationExceptions.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Application.Exceptions
+{
+    public class ValidationExceptions : Exception
+    {
+        public IDictionary<string, string[]> Errors { get; }
+
+        public ValidationExceptions(IEnumerable<ValidationFailure> failures)
+            : base("Validation errors occurred.")
+        {
+            Errors = failures
+                .GroupBy(e => e.PropertyName)
+                .ToDictionary(k => k.Key, v => v.Select(x => x.ErrorMessage).ToArray());
+        }
+    }
+}
